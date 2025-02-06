c86bc9e274858f5b13a87a3d0c4095ce856dc6e4
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-07 00:42:57 +0500

    Refactor layout to use Inter font and update metadata; add logo image to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div className='flex flex-col items-center justify-center h-screen space-y-4'>
       <h1 className='text-3xl font-bold'>YouTube Clone</h1>
       <Button variant={'default'}>Click me</Button>
+      <Image src={'/logo.svg'} alt='logo' width={200} height={200} />
     </div>
   )
 }
