fe48f37a6697a0a59a48ef355509de1016f92be7
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-07 16:28:12 +0500

    Refactor VideoId component to use async params and add FeedLayout for consistent styling

--- app/feed/[videoId]/page.tsx
+++ app/feed/[videoId]/page.tsx
@@ -1,16 +1,19 @@
-'use client'
-
-import { useParams } from 'next/navigation'
 import React from 'react'
 
-const VideoId = () => {
-    const { videoId } = useParams()
-  return (
-    <div className='flex flex-col items-center justify-center h-screen space-y-4'>
-      <h1 className='text-3xl font-bold'>Video ID Page</h1>
-      <p className='text-lg'>Displaying content for video ID: {videoId}</p>
-    </div>
-  )
+interface VideoIdProps {
+    params: Promise<{ videoId: string }>
+}
+
+const VideoId = async ({ params }: VideoIdProps) => {
+    const { videoId } = await params;
+
+    return (
+        <div >
+            <h1 className='text-3xl font-bold'>Video ID Page</h1>
+            <p className='text-lg'>Displaying content for video ID: {videoId}</p>
+            
+        </div>
+    )
 }
 
 export default VideoId
--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Feed = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen space-y-4'>
+    <div>
         <h1 className='text-3xl font-bold'>Feed</h1>
         <p className='text-lg'>Welcome to the feed section. Stay tuned for updates!</p>
     </div>
