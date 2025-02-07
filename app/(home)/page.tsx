d5e75d68063193a09ba78e3a14e293f706317d37
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-07 18:27:07 +0500

    Refactor Home component and implement HomeNavbar with sidebar integration

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import React from 'react'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen space-y-4'>
-      <h1 className='text-3xl font-bold'>YouTube Clone</h1>
-      <Button variant={'default'}>Click me</Button>
       <Image src={'/logo.svg'} alt='logo' width={200} height={200} />
     </div>
   )
--- app/modules/home/ui/components/home-navbar/index.tsx
+++ app/modules/home/ui/components/home-navbar/index.tsx
@@ -1,7 +1,22 @@
+import { SidebarTrigger } from '@/components/ui/sidebar'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export const HomeNavbar = () => {
-  return (
-    <div>HomeNavbar</div>
-  )
+    return (
+        <nav className='fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50'>
+            <div className='flex items-center gap-4 w-full'>
+                <div className='flex items-center flex-shrink-0'>
+                    <SidebarTrigger />
+                    <Link href={'/'}>
+                        <div className='p-4 flex items-center gap-1'>
+                            <Image src='/logo.svg' alt='Logo' width={32} height={32} />
+                            <p className='text-xl font-semibold tracking-tight'>NewTube</p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
 }
