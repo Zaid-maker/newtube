f475b55c5c572e927b3668c0189e76fd1068dbb4
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-10 23:47:44 +0500

    Remove logo image and update Home component to indicate future video loading

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen space-y-4'>
-      <Image src={'/logo.svg'} alt='logo' width={200} height={200} />
+    <div className='pl-3'>
+      I will load videos in future
     </div>
   )
 }
