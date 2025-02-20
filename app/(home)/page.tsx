9ed9bb0d8622db1dd5c21565329b537b10707405
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-21 03:24:25 +0500

    Set runtime to Node.js for Home page component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,8 @@ import { HydrateClient, trpc } from "@/trpc/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
+export const runtime = "nodejs";
+
 export default async function Home() {
   void trpc.hello.prefetch({
     text: "Antonio",
