131327803e67126a6b272cd9a176e48930361c1f
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-22 03:07:44 +0500

    Refactor Home component to remove PageClient and implement dynamic runtime; update to use HomeView for rendering

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,23 @@
 import { HydrateClient, trpc } from "@/trpc/server";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-import { PageClient } from "./client";
 
-export const runtime = "nodejs";
+export const runtime = "force-dynamic";
 
-export default async function Home() {
-  try {
-    await trpc.categories.getMany.prefetch();
-  } catch (error) {
-    console.error("Error during prefetch:", error);
-    // You might choose to handle the error (e.g., return a fallback UI or rethrow)
-  }
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>;
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
+
+  void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ErrorBoundary fallback={<p>Something went wrong</p>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
-}
+};
+
+export default Page;
