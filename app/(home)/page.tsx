0a2347d338cbfd965858e6aa576eefaf745bf213
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-22 03:01:56 +0500

    Fetch categories data in Home component and create PageClient for rendering

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,13 @@
 import { HydrateClient, trpc } from "@/trpc/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { PageClient } from "./client";
 
 export const runtime = "nodejs";
 
 export default async function Home() {
   try {
-    await trpc.hello.prefetch({
-      text: "Antonio",
-    });
+    await trpc.categories.getMany.prefetch();
   } catch (error) {
     console.error("Error during prefetch:", error);
     // You might choose to handle the error (e.g., return a fallback UI or rethrow)
@@ -18,7 +17,7 @@ export default async function Home() {
     <HydrateClient>
       <Suspense fallback={<p>Loading...</p>}>
         <ErrorBoundary fallback={<p>Something went wrong</p>}>
-          Page component render
+          <PageClient />
         </ErrorBoundary>
       </Suspense>
     </HydrateClient>
