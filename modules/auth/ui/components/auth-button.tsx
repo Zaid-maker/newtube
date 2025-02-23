bfae793956330d53ad6bda70c0560a4761416afd
Zaid Hafeez <53424436+Zaid-maker@users.noreply.github.com>
2025-02-23 18:48:15 +0000

    Enhance AuthButton to include a link to the studio for signed-in users

--- modules/auth/ui/components/auth-button.tsx
+++ modules/auth/ui/components/auth-button.tsx
@@ -1,27 +1,35 @@
-'use client'
+"use client";
 
-import { Button } from '@/components/ui/button'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-import { UserCircleIcon } from 'lucide-react'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ClapperboardIcon, UserCircleIcon } from "lucide-react";
+import React from "react";
 
 export const AuthButton = () => {
-    return (
-        <>
-            <SignedIn>
-                <UserButton />
-            </SignedIn>
-            <SignedOut>
-                <SignInButton mode='modal'>
-                    <Button
-                        variant={'outline'}
-                        className='px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none []'
-                    >
-                        <UserCircleIcon />
-                        Sign In
-                    </Button>
-                </SignInButton>
-            </SignedOut>
-        </>
-    )
-}
+  return (
+    <>
+      <SignedIn>
+        <UserButton>
+          <UserButton.MenuItems>
+            <UserButton.Link
+              label="studio"
+              href="/studio"
+              labelIcon={<ClapperboardIcon className="size-4" />}
+            />
+          </UserButton.MenuItems>
+        </UserButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant={"outline"}
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none []"
+          >
+            <UserCircleIcon />
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
+  );
+};
