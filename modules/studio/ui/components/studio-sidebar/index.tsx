9b30cb1e9dd49d943f32826649c6b561288cf58e
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-24 00:18:32 +0500

    Refactor StudioNavbar and StudioSidebar components for improved structure and consistency

--- modules/studio/ui/components/studio-sidebar/index.tsx
+++ modules/studio/ui/components/studio-sidebar/index.tsx
@@ -1,17 +1,17 @@
-import { Sidebar, SidebarContent } from '@/components/ui/sidebar'
-import React from 'react'
-import { MainSection } from './main-section'
-import { Separator } from '@/components/ui/separator'
-import { PersonalSection } from './personal-section'
+import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import React from "react";
+import { MainSection } from "./main-section";
+import { Separator } from "@/components/ui/separator";
+import { PersonalSection } from "./personal-section";
 
 export const StudioSidebar = () => {
   return (
-    <Sidebar className='pt-16 z-40 border-none' collapsible='icon'>
-      <SidebarContent className='bg-background'>
+    <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+      <SidebarContent className="bg-background">
         <MainSection />
         <Separator />
         <PersonalSection />
       </SidebarContent>
     </Sidebar>
-  )
-}
+  );
+};
