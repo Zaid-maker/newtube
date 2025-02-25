1a0c69902f9c4e114e449d20bf14a4aedd9f9da3
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-26 02:17:19 +0500

    Enhance StudioSidebar with new menu structure and exit functionality

--- modules/studio/ui/components/studio-sidebar/index.tsx
+++ modules/studio/ui/components/studio-sidebar/index.tsx
@@ -1,16 +1,34 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
-import React from "react";
-import { MainSection } from "./main-section";
 import { Separator } from "@/components/ui/separator";
-import { PersonalSection } from "./personal-section";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { MainSection } from "./main-section";
+import Link from "next/link";
+import { LogOutIcon } from "lucide-react";
 
 export const StudioSidebar = () => {
   return (
-    <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+    <Sidebar className="pt-16 z-40" collapsible="icon">
       <SidebarContent className="bg-background">
         <MainSection />
         <Separator />
-        <PersonalSection />
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton tooltip={"Exit Studio"} asChild>
+                <Link href="/">
+                  <LogOutIcon className="size-5" />
+                  <span className="text-sm">Exit Studio</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
     </Sidebar>
   );
