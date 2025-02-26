8ba8e1127e4e07a1e3a3bbba6d8aba66ec7951c8
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-26 16:30:25 +0500

    feat(tests): refactor test setup by removing old setup files and integrating jest-dom matchers

--- __tests__/setup.ts
+++
@@ -1,3 +0,0 @@
-import { expect, test } from "bun:test";
-
-export { expect, test };
--- test/setup.ts
+++ test/setup.ts
@@ -1,7 +1,13 @@
 import { expect, test, afterEach } from "bun:test";
 import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { TestWrapper } from "./test-utils";
-import "@testing-library/jest-dom/matchers";
+
+// Extend jest matchers
+import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
+declare module "bun:test" {
+  interface Matchers<R> extends TestingLibraryMatchers<typeof expect, R> {}
+}
 
 // Cleanup after each test
 afterEach(() => {
