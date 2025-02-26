0c8f6bc5baf618b0ff32746ff85ce3777ace83e2
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-02-26 16:38:39 +0500

    feat(tests): extend bun:test Expect interface with additional custom matchers

--- test/matchers.ts
+++ test/matchers.ts
@@ -5,5 +5,22 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 Object.assign(expect.prototype, matchers);
 
 declare module "bun:test" {
-  interface Expect extends matchers.TestingLibraryMatchers<any, void> {}
+  interface Expect {
+    toBeInTheDocument(): boolean;
+    toBeVisible(): boolean;
+    toBeDisabled(): boolean;
+    toBeEnabled(): boolean;
+    toBeEmptyDOMElement(): boolean;
+    toHaveTextContent(text: string | RegExp): boolean;
+    toHaveValue(value: string | string[] | number | null): boolean;
+    toHaveStyle(css: Partial<CSSStyleDeclaration>): boolean;
+    toHaveClass(...classNames: string[]): boolean;
+    toHaveFocus(): boolean;
+    toBeChecked(): boolean;
+    toBeEmpty(): boolean;
+    toContainElement(element: HTMLElement | null): boolean;
+    toContainHTML(html: string): boolean;
+    toHaveAttribute(attr: string, value?: string): boolean;
+    toHaveDescription(text?: string | RegExp): boolean;
+  }
 }
