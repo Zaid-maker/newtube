b7cfab6609b9d091211423e5f57a8a31810e9262
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-03 01:23:38 +0500

    feat(webhook): handle video.asset.ready event and store thumbnail URL in database

--- app/api/videos/webhook/route.ts
+++ app/api/videos/webhook/route.ts
@@ -58,6 +58,31 @@ export const POST = async (request: Request) => {
         .where(eq(videos.muxUploadId, data.upload_id));
 
       break;
+
+    case "video.asset.ready": {
+      const data = payload.data as VideoAssetReadyWebhookEvent["data"];
+      const playbackId = data.playback_ids?.[0].id;
+
+      if (!data.upload_id) {
+        return new Response("No upload ID found", { status: 400 });
+      }
+
+      if (!playbackId) {
+        return new Response("No playback ID found", { status: 400 });
+      }
+
+      const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
+
+      await db
+        .update(videos)
+        .set({
+          muxStatus: data.status,
+          muxPlaybackId: playbackId,
+          muxAssetId: data.id,
+          thumbnailUrl,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id));
+    }
   }
 
   return new Response("Webhook received", { status: 200 });
