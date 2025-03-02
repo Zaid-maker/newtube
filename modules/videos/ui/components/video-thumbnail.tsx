b27642e64cdf94bc8212612fc1fc011e252f9b7d
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-03 01:42:07 +0500

    feat(video-thumbnail): add imageUrl prop to VideoThumbnail component for dynamic thumbnail display

--- modules/videos/ui/components/video-thumbnail.tsx
+++ modules/videos/ui/components/video-thumbnail.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
-import React from "react";
 
-export const VideoThumbnail = () => {
+interface VideoThumbnailProps {
+  imageUrl?: string;
+}
+
+export const VideoThumbnail = ({ imageUrl }: VideoThumbnailProps) => {
   return (
     <div className="relative">
       {/* Thumbnail Wrapper */}
       <div className="relative wfull overflow-hidden rounded-xl aspect-video">
         <Image
-          src={"/placeholder.svg"}
+          src={imageUrl ??"/placeholder.svg"}
           alt="Thumbnail"
           fill
           className="h-full w-full object-cover"
