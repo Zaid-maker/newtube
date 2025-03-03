364d8c4fd6434e5435ac0ed8a5169ea5ab1a3ddd
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-04 03:22:24 +0500

    feat(video-thumbnail): format duration display in VideoThumbnail component

--- modules/videos/ui/components/video-thumbnail.tsx
+++ modules/videos/ui/components/video-thumbnail.tsx
@@ -1,3 +1,4 @@
+import { formatDuration } from "@/lib/utils";
 import Image from "next/image";
 
 interface VideoThumbnailProps {
@@ -32,7 +33,7 @@ export const VideoThumbnail = ({
       </div>
 
       <div className="absolute bottom-2 right-2 px-1 py-0.5 rounded bg-black/80 text-white text-xs font-medium">
-        {duration}
+        {formatDuration(duration)}
       </div>
     </div>
   );
