9d28dd66e7631158895064cebf57dd33ed0e916d
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-03 17:15:32 +0500

    feat(video-thumbnail): update alt text for images to use title prop

--- modules/videos/ui/components/video-thumbnail.tsx
+++ modules/videos/ui/components/video-thumbnail.tsx
@@ -17,13 +17,13 @@ export const VideoThumbnail = ({
       <div className="relative wfull overflow-hidden rounded-xl aspect-video">
         <Image
           src={imageUrl ?? "/placeholder.svg"}
-          alt="Thumbnail"
+          alt={title}
           fill
           className="h-full w-full object-cover group-hover:opacity-0"
         />
         <Image
           src={previewUrl ?? "/placeholder.svg"}
-          alt="Thumbnail"
+          alt={title}
           fill
           className="h-full w-full object-cover opacity-0 group-hover:opacity-100"
         />
