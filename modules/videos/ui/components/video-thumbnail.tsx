996fc7265fad3cb6bf71d0e3d1e5e2e8eb0d704b
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-03 17:11:05 +0500

    feat(video-thumbnail): add title and preview URL props to VideoThumbnail component

--- modules/videos/ui/components/video-thumbnail.tsx
+++ modules/videos/ui/components/video-thumbnail.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 
 interface VideoThumbnailProps {
+  title: string;
   imageUrl?: string | null;
+  previewUrl?: string | null;
 }
 
-export const VideoThumbnail = ({ imageUrl }: VideoThumbnailProps) => {
+export const VideoThumbnail = ({
+  title,
+  imageUrl,
+  previewUrl,
+}: VideoThumbnailProps) => {
   return (
     <div className="relative">
       {/* Thumbnail Wrapper */}
