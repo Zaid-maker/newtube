3a37e37e30f91828188f73ad9eebe384f9894017
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-05 00:06:10 +0500

    feat(video-section): add visibility icons for private and public videos

--- modules/studio/ui/sections/video-section.tsx
+++ modules/studio/ui/sections/video-section.tsx
@@ -17,6 +17,7 @@ import Link from "next/link";
 import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
 import { snakeCaseToTitle } from "@/lib/utils";
 import { format } from "date-fns";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 export const VideoSection = () => {
   return (
@@ -85,6 +86,11 @@ const VideoSectionSuspense = () => {
                     </TableCell>
                     <TableCell>
                       <div className="flex items-center">
+                        {video.visibility === "private" ? (
+                          <LockIcon className="size-4 mr-2" />
+                        ) : (
+                          <Globe2Icon className="size-4 mr-2" />
+                        )}
                         {snakeCaseToTitle(video.visibility)}
                       </div>
                     </TableCell>
