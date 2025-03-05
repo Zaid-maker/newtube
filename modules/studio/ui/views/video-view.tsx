c7309f288b7fb9142932e2bcc54371a41d4bacc7
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-06 01:19:53 +0500

    feat(video-view): add videoId prop to VideoView component for dynamic rendering

--- modules/studio/ui/views/video-view.tsx
+++ modules/studio/ui/views/video-view.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
-export const VideoView = () => {
-  return (
-    <div>VideoView</div>
-  )
+interface PageProps {
+  videoId: string;
 }
+
+export const VideoView = ({ videoId }: PageProps) => {
+  return <div className="px-4 pt-2.6 max-w-screen-lg">{videoId}</div>;
+};
