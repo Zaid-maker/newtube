f8254876825809e7095200bdf1e44d6d24377b95
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-06 15:23:33 +0500

    feat(form-section): add suspense and error boundary for video loading

--- modules/studio/ui/sections/form-section.tsx
+++ modules/studio/ui/sections/form-section.tsx
@@ -1,13 +1,32 @@
 "use client";
 
 import { trpc } from "@/trpc/client";
-import React from "react";
+import React, { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface FormSectionProps {
   videoId: string;
 }
 
 export const FormSection = ({ videoId }: FormSectionProps) => {
+  return (
+    <Suspense fallback={<FormSectionSkeleton />}>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <FormSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const FormSectionSkeleton = () => {
+  return (
+    <div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
   const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId });
 
   return <div>{JSON.stringify(video)}</div>;
