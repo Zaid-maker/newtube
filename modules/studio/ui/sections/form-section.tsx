981154c2fc8268dddfadb29f99131142ab91c65c
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-06 22:29:41 +0500

    update

--- modules/studio/ui/sections/form-section.tsx
+++ modules/studio/ui/sections/form-section.tsx
@@ -13,6 +13,9 @@ import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { videoUpdateSchema } from "@/db/schema";
+import { Form } from "@/components/ui/form";
 
 interface FormSectionProps {
   videoId: string;
@@ -39,36 +42,45 @@ const FormSectionSkeleton = () => {
 const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
   const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId });
 
-  const form = useForm({
+  const form = useForm<z.infer<typeof videoUpdateSchema>>({
+    resolver: zodResolver(videoUpdateSchema),
     defaultValues: video,
   });
 
+  const onSubmit = async (data: z.infer<typeof videoUpdateSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <div className="flex items-center justify-between mb-6">
-      <div>
-        <h1 className="text-2xl font-bold">Video Details</h1>
-        <p className="text-xs text-muted-foreground">
-          Mange your video details
-        </p>
-      </div>
-      <div className="flex items-center gap-x-2">
-        <Button type="submit" disabled={false}>
-          Save
-        </Button>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <MoreVertical />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex items-center justify-between mb-6">
+          <div>
+            <h1 className="text-2xl font-bold">Video Details</h1>
+            <p className="text-xs text-muted-foreground">
+              Mange your video details
+            </p>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <Button type="submit" disabled={false}>
+              Save
             </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem>
-              <TrashIcon className="size-4 mr-2" />
-              Delete
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <MoreVertical />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                  <TrashIcon className="size-4 mr-2" />
+                  Delete
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </form>
+    </Form>
   );
 };
