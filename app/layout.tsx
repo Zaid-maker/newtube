13e9f733430852ca1de94087b48b07bef0aabd75
Zaid-maker <53424436+Zaid-maker@users.noreply.github.com>
2025-03-23 13:29:42 +0500

    feat: enable rich colors for Toaster component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <html lang="en">
         <body className="antialiased">
           <TRPCProvider>
-            <Toaster />
+            <Toaster richColors />
             {children}
           </TRPCProvider>
         </body>
